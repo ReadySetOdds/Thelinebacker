43a76af17adc0d99b73d28c3478cc7fea18f55a0
knathanwilson <knathanwilson@gmail.com>
2019-12-17 09:10:39 -0500

    Data is extracting, now ready to put into database

--- config.py
+++
@@ -1,8 +0,0 @@
-
-# sign in
-linebacker_username = 'rmarshallsmith@hotmail.com'
-linebacker_password = 'football2020'
-
-# digital ocear
-'dexter@readysetodds.com'
-'Henk123!'
--- linebacker_scraper.py
+++ linebacker_scraper.py
@@ -1,16 +1,33 @@
 
 # import dependencies
-import config
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from html2text import html2text as htt
 
-# settings
+# constants
+linebacker_username = 'rmarshallsmith@hotmail.com'
+linebacker_password = 'football2020'
 signin_url = 'https://www.thelinebacker.com/signin'
-games_url = 'https://www.thelinebacker.com/{}/games/{}'
+initial_games_url = 'https://www.thelinebacker.com/{}/games'
+games_url = initial_games_url + '/{}'
 odds_url = 'https://www.thelinebacker.com/odds/{}'
 best_bets_column_count = 8
+element_timeout = 30
+weeks_dropdown_class = 'sc-iFMziU.lkmqRa'
+games_table_class = 'sc-RbTVP.beCBks'
+odds_table_class = 'chalk-event'
+table_indices = (0, 2, 10, 12, 16, 18, 26, 28, 36, 38, 44, 48, 50, 56, 60)
 
 # variables
 driver = None
+
+
+# helper
+def wait_for_element(class_name, by=By.CLASS_NAME):
+	WebDriverWait(driver, element_timeout).until(EC.presence_of_element_located((by, class_name)))
+
 
 # main
 if __name__ == '__main__':
@@ -23,15 +40,16 @@ if __name__ == '__main__':
 	for element in driver.find_elements_by_tag_name('input'):
 		name = element.get_attribute('name')
 		if name == 'email':
-			element.send_keys(config.linebacker_username)
+			element.send_keys(linebacker_username)
 		elif name == 'password':
-			element.send_keys(config.linebacker_password)
+			element.send_keys(linebacker_password)
 	for element in driver.find_elements_by_tag_name('button'):
 		if element.text == 'Log in':
 			element.click()
 			break
 	
 	# get best bets
+	wait_for_element('table', by=By.TAG_NAME)
 	while True:
 		element_found = False
 		for element in driver.find_elements_by_tag_name('table'):
@@ -43,18 +61,38 @@ if __name__ == '__main__':
 	row = []
 	for cell in cells[best_bets_column_count:]:
 		row.append(cell)
-		if len(row) > best_bets_column_count: pass
-	
-	'''
+		if len(row) > best_bets_column_count: pass#DATAOP
+
 	# go to sports
 	for sport in ('NFL', 'NCAAF', 'NBA', 'NCAAB', 'MLB', 'NHL'):
-		
-		# games
-		page_number = 1
+
+		# initalize games page
+		driver.get(initial_games_url.format(sport.lower()))
+		wait_for_element(weeks_dropdown_class)
+
+		# get games data
+		last_url = ''
+		week = 1
 		while True:
-			driver.get(games_url.format(sport, page_number))
-			for element in driver.find_elements_by_tag_name('tbody'):
-				for child in element.find_elements_by_tag_name('td'):
-					print(child.find_attribute('data-cy'))
-			exit()
-	'''
+
+			# go to page
+			driver.get(games_url.format(sport.lower(), week))
+			wait_for_element(games_table_class)
+			if driver.current_url == last_url: break
+
+			# go through tables
+			for table in driver.find_elements_by_class_name(games_table_class):
+				data = htt(table.get_attribute('innerHTML')).split('\n')
+				for index in table_indices: pass#DATAOP
+
+			# update
+			week += 1
+			last_url = driver.current_url
+
+		# get odds data
+		driver.get(odds_url.format(sport.lower()))
+		wait_for_element(odds_table_class)
+		for table in driver.find_elements_by_class_name(odds_table_class):
+			data = htt(table.text).split()
+			#DATAOP
+
